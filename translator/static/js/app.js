ab77b92bc367186d037b0773d7bd78b84dcd804a
Faizal <faizal.abdul.tif420@polban.ac.id>
2022-05-24 00:41:53 +0700

    Menambah Login Dan Registrasi
    - Menambahkan Fitur Login Dan Registrasi

--- translator/static/js/app.js
+++ translator/static/js/app.js
@@ -61,3 +61,20 @@ function showFile(){
     dragText.textContent = "Drag & Drop to Upload File";
   }
 }
+
+(function($) {
+
+	"use strict";
+
+	$(".toggle-password").click(function() {
+
+  $(this).toggleClass("fa-eye fa-eye-slash");
+  var input = $($(this).attr("toggle"));
+  if (input.attr("type") == "password") {
+    input.attr("type", "text");
+  } else {
+    input.attr("type", "password");
+  }
+});
+
+})(jQuery);
